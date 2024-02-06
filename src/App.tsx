4672c51badb1de88f9ed80976c893288fa1b1f4a
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2024-02-07 03:36:13 +0500

    package installation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import HomePage from "./components/home-page";
 function App() {
   return (
     <div>
-      <div className="text-3xl font-bold underline text-[red]">Hello world</div>
       <HomePage />
     </div>
   );
