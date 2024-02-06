ad30c6f2e8b789a84815a447e481bc84c9ef8e36
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2024-02-07 03:28:54 +0500

    working on installing tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import HomePage from "./components/home-page";
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <div className="text-3xl font-bold underline text-[red]">Hello world</div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
