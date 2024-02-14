e78876695ab770823d643f4a68c5e120e641e19e
Abuzar Rahim <abuzar.rahim70@hotmail.com>
2024-02-14 05:21:52 +0500

    create other services component

--- src/components/home-page/commons/custom-others/custom-others.tsx
+++ src/components/home-page/commons/custom-others/custom-others.tsx
@@ -1,7 +1,19 @@
 import { Flex, Typography, theme } from "antd";
-//import Img1 from "../../../../assets/Images/services/electrical.png";
+import Img1 from "../../../../assets/Images/services/electrical.png";
+import Img2 from "../../../../assets/Images/services/softwar.png";
+import Img3 from "../../../../assets/Images/services/technology.png";
+import Img4 from "../../../../assets/Images/services/mechanical.png";
+import Img5 from "../../../../assets/Images/services/manufacturing.png";
+import Img6 from "../../../../assets/Images/services/energy.png";
+import { useState } from "react";
 const CustomOthers = () => {
   const { token } = theme.useToken();
+  const [eeImage, setEeImage] = useState(false);
+  const [seImage, setSeImage] = useState(false);
+  const [tImage, setTImage] = useState(false);
+  const [meImage, setMeImage] = useState(false);
+  const [mImage, setMImage] = useState(false);
+  const [eImage, setEImage] = useState(false);
   return (
     <Flex
       className="w-full min-h-[300px] xs:max-sm:flex-col"
@@ -41,8 +53,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              backgroundImage: eeImage ? `url(${Img1})` : "",
+            }}
+            className={` select-none text-center text-[#efc75e] hover:text-white `}
+            onMouseEnter={() => {
+              setEeImage(true);
+            }}
+            onMouseLeave={() => setEeImage(false)}
           >
             <Typography.Text
               style={{
@@ -58,8 +77,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              backgroundImage: meImage ? `url(${Img4})` : "",
+            }}
+            className={`select-none text-center text-[#efc75e] hover:text-white bg-transparent `}
+            onMouseEnter={() => {
+              setMeImage(true);
+            }}
+            onMouseLeave={() => setMeImage(false)}
           >
             <Typography.Text
               style={{
@@ -77,8 +103,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              backgroundImage: seImage ? `url(${Img2})` : "",
+            }}
+            className={` select-none text-center text-[#efc75e] hover:text-white bg-transparent `}
+            onMouseEnter={() => {
+              setSeImage(true);
+            }}
+            onMouseLeave={() => setSeImage(false)}
           >
             <Typography.Text
               style={{
@@ -94,8 +127,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              background: eImage ? `url(${Img6})` : "",
+            }}
+            className={`  text-[#efc75e] text-center hover:text-white bg-transparent `}
+            onMouseEnter={() => {
+              setEImage(true);
+            }}
+            onMouseLeave={() => setEImage(false)}
           >
             <Typography.Text
               style={{
@@ -113,8 +153,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              backgroundImage: tImage ? `url(${Img3})` : "",
+            }}
+            className={` select-none text-center text-[#efc75e] hover:text-white bg-transparent `}
+            onMouseEnter={() => {
+              setTImage(true);
+            }}
+            onMouseLeave={() => setTImage(false)}
           >
             <Typography.Text
               style={{
@@ -130,8 +177,15 @@ const CustomOthers = () => {
             flex={1}
             justify="center"
             align="center"
-            style={{ border: "1px solid #efc75e" }}
-            className={`  text-[#efc75e] hover:text-white bg-transparent `}
+            style={{
+              border: "1px solid #efc75e",
+              backgroundImage: mImage ? `url(${Img5})` : "",
+            }}
+            className={`select-none text-center text-[#efc75e] hover:text-white bg-transparent `}
+            onMouseEnter={() => {
+              setMImage(true);
+            }}
+            onMouseLeave={() => setMImage(false)}
           >
             <Typography.Text
               style={{
